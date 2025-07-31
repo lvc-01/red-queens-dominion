42cc30dd8b3b5c4725db04a4246d0e30569a0856
leandrivancittert <leandri.vancittert@psybergate.co.za>
2025-07-31 12:34:45 +0200

    simple key added

---
+++ red-queen's-dominion/scripts/items/key.gd
@@ -0,0 +1,16 @@
+extends Item
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_body_entered(body):
+	if body is Player:
+		print("Key collected")
+		is_picked_up = true
+		queue_free()
