9378cdced006769cc8e8ad74a3795f72436707ac
hendrikmuller <hendrik.muller@psybergate.co.za>
2025-08-02 11:15:46 +0200

    reset and pause
    # Conflicts:
    #	red-queen's-dominion/Map/Map/Map.tscn
    #	red-queen's-dominion/project.godot
    #	red-queen's-dominion/scripts/player/player.gd

--- red-queen's-dominion/scripts/player/player.gd
+++ red-queen's-dominion/scripts/player/player.gd
@@ -1,7 +1,7 @@
 class_name Player
 extends Entity
 
-var reset_pos: Vector3
+var reset_pos = Vector3(0, 0, 0)
 
 const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
@@ -11,7 +11,7 @@ const SENSITIVITY = 0.003
 const BOB_FREQ = 2.0
 const BOB_AMP = 0.08
 var t_bob = 0.0
-
+ 
 #Inventory
 var inventory_keys:Array[String] =[]
 
@@ -20,8 +20,10 @@ var inventory_keys:Array[String] =[]
 
 #Mouse movement
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	reset_pos = position
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	process_mode = Node.PROCESS_MODE_PAUSABLE
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -30,22 +32,18 @@ func _unhandled_input(event: InputEvent) -> void:
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	#Sprint
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -55,7 +53,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0.0
 		velocity.z = 0.0
 
-#bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
 
