1e7be493e0d28e5aa2fe6fccfa8bbf5dfaf20e33
hendrikmuller <hendrik.muller@psybergate.co.za>
2025-08-02 11:18:43 +0200

    rebasing

--- red-queen's-dominion/scripts/player/player.gd
+++ red-queen's-dominion/scripts/player/player.gd
@@ -1,8 +1,6 @@
 class_name Player
 extends Entity
 
-var reset_pos = Vector3(0, 0, 0)
-
 const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.003
@@ -11,19 +9,13 @@ const SENSITIVITY = 0.003
 const BOB_FREQ = 2.0
 const BOB_AMP = 0.08
 var t_bob = 0.0
- 
-#Inventory
-var inventory_keys:Array[String] =[]
 
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
 
 #Mouse movement
 func _ready():
-	reset_pos = position
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	process_mode = Node.PROCESS_MODE_PAUSABLE
-
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -32,18 +24,22 @@ func _unhandled_input(event: InputEvent) -> void:
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 func _physics_process(delta: float) -> void:
-
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	#Sprint
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
 
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -53,6 +49,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0.0
 		velocity.z = 0.0
 
+#bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
 
