414633af194a150a4a91440ea35b850060f66275
leandrivancittert <leandri.vancittert@psybergate.co.za>
2025-08-02 09:54:15 +0200

    queen sometimes forgets how doorways work, still floats menacingly

--- red-queen's-dominion/scripts/queen.gd
+++ red-queen's-dominion/scripts/queen.gd
@@ -5,17 +5,14 @@ var player_spotted:= false
 var target_position: Vector3
 
 @onready var nav_agent = $NavigationAgent3D
-@onready var animation_player = $queen/AnimationPlayer
 @onready var player = get_tree().get_first_node_in_group("Player")
 @onready var patrol_timer = $PatrolTimer
 
 
 func _ready():
-	print(position)
 	speed = WALK_SPEED
-	#animation_player.play("alien_xenos_drone_SK_Xenos_Drone_skeleton|idle")
 	#update later and include y for stairs
-	target_position = Vector3(randf_range(-42, 150), 12.264, randf_range(-37, 1))
+	target_position = Vector3(randf_range(-42, 150), 11, randf_range(-37, 1))
 	patrol_timer.start(60)
 
 
@@ -23,6 +20,9 @@ func _physics_process(delta):
 	print(str(position) + " position")
 	print(str(target_position) + " target position")
 	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
 	if !player_spotted:
 		patrol()
 	else:
@@ -33,21 +33,15 @@ func _physics_process(delta):
 	move_and_slide()
 	
 func patrol():
-	#print("Queen is looking for Alice.")
-	animation_player.play("alien_xenos_drone_SK_Xenos_Drone_skeleton|walk")
 	#look_at((nav_agent.get_next_path_position() - global_position).normalized(), Vector3.UP)
 	nav_agent.target_position = target_position
-	#print(position)
-	#print(target_position)
+	if(abs(target_position.x - position.x)<=5 and abs(target_position.z - position.z)<=5):
+		calculate_next_position()
 	
 	
 func chase_player():
-	#print("Queen is chasing Alice.")
-	animation_player.play("alien_xenos_drone_SK_Xenos_Drone_skeleton|run1")
 	#look_at(player.position, Vector3.UP)
 	nav_agent.target_position = player.global_position
-	if(abs(target_position.x - global_position.x)<=5 and abs(target_position.z - global_position.z)<=5):
-		calculate_next_position()
 
 
 func _on_detection_area_body_entered(body):
@@ -73,9 +67,10 @@ func _on_patrol_timer_timeout():
 	
 	
 func calculate_next_position():
+	print("Calculating new path...")
 	var player_position = player.global_position
 	target_position = Vector3(randf_range(player_position.x - 40, player_position.x + 40), 		
-	12.264, randf_range(player_position.z - 40, player_position.z + 40))
+	11, randf_range(player_position.z - 40, player_position.z + 40))
 	
 	#likely need to add y to the mix later, update to account for world boundaries
 	clamp(target_position.x, -42, 150)
